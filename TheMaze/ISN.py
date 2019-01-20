31b30a36badbe11aea30f23ee6aac2e25ab54498
Sopetip <43827093+Sopetip@users.noreply.github.com>
2019-01-20 23:36:29 +0100

    Update ISN.py

--- TheMaze/ISN.py
+++ TheMaze/ISN.py
@@ -6,48 +6,60 @@ a = randint(13, 14)
 fenetre = Tk()
 
 w, h = fenetre.winfo_screenwidth(), fenetre.winfo_screenheight()
-img = PhotoImage(file='themaze.png')                                       #on importe le skin du jeu
+img = PhotoImage(file='themaze.png')  # on importe le skin du jeu
 img2 = PhotoImage(file='fin2.png')
 
 can = Canvas(fenetre, height=700, width=800, background='black')
-can.create_image(0, 0, image=img, anchor='nw')              #on définit le canvas et on set les images pour faire le jeu
+can.create_image(0, 0, image=img, anchor='nw')  # on définit le canvas et on set les images pour faire le jeu
 can.create_image(0, 150, image=img2, anchor='nw')
 
-player = can.create_rectangle(213, 228, 237, 253, outline='blue', fill='blue')  #on crée le player
+player = can.create_rectangle(213, 228, 237, 253, outline='blue', fill='blue')  # on crée le player
 
 baseCoords = (213, 228, 237, 253)
-PCoords = [213, 228, 237, 253]                  #on définit les coordonnées de base pour plus tard
+PCoords = [213, 228, 237, 253]  # on définit les coordonnées de base pour plus tard
+print(PCoords)
 
 lvl1 = ['d', 'd', 'r', 'u', 'u', 'r', 'r', 'r', 'r', 'r', 'd', 'l', 'd', 'l', 'u', 'l', 'd', 'd', 'd', 'd', 'l', 'l',
         'l', 'd', 'd', 'r', 'u''r', 'd', 'r', 'u', 'r', 'u', 'r', 'd', 'd', 'l', 'd']
-                            #les niveaux
-lvl0 = ['d', 'd', 'd', 'd', 'd', 'd', 'd', 'd', 'r', 'r', 'r', 'r']
+# les niveaux
+lvl0 = ['d', 'd', 'd', 'd', 'd', 'd', 'd', 'r', 'r', 'r', 'r']
 win = False
 path = []
-currentlvl = lvl0
+currentlvl = lvl1 
+error = False
 
 
 def reset():
     print("PANIC")
     path[:] = []
-    PCoords[0] = 213
-    PCoords[1] = 228
-    PCoords[2] = 237
-    PCoords[3] = 253    
-    can.coords(player, PCoords[0], PCoords[1], PCoords[2], PCoords[3])
+    can.itemconfig(player, outline='blue',fill='blue')
     pass
 
 
-def check():
+def check():  # on check par rapport au maze choisi
+    global error
+    global win
     if len(path) == 0:
-        return [None]   #on check par rapport au maze choisi
+        return [None]
+
+    if error == True:
+        global PCoords
+        if PCoords ==[213,228,237,253]:
+            error = False
+            reset()
+        return [None]
+
     i = len(path) - 1
+
     if path[i] != currentlvl[i]:
-        can.itemconfig(player, outline = 'red', fill = 'red')
-    	
+        can.itemconfig(player, outline='red', fill='red')
+        error = True
+    if len(path) == len(currentlvl) and error == False:
+        win = True
+        print("GG")
 
 
-def movedown(event):                        # les 4 fonctions de mouvements qui sont quasiment les mêmes
+def movedown(event):  # les 4 fonctions de mouvements qui sont quasiment les mêmes
     PCoords[1] = PCoords[1] + 50
     PCoords[3] = PCoords[3] + 50
     path.append('d')
@@ -57,7 +69,7 @@ def movedown(event):                        # les 4 fonctions de mouvements qui 
         PCoords[3] = PCoords[3] - 50
         del path[-1]
     can.coords(player, PCoords[0], PCoords[1], PCoords[2], PCoords[3])
-    print(path)
+    print(path,PCoords,baseCoords)
     check()
 
 
@@ -71,7 +83,7 @@ def moveup(event):
         PCoords[3] = PCoords[3] + 50
         del path[-1]
     can.coords(player, PCoords[0], PCoords[1], PCoords[2], PCoords[3])
-    print(path)
+    print(path,PCoords,baseCoords)
     check()
 
 
@@ -85,7 +97,7 @@ def moveleft(event):
         PCoords[2] = PCoords[2] + 50
         del path[-1]
     can.coords(player, PCoords[0], PCoords[1], PCoords[2], PCoords[3])
-    print(path)
+    print(path,PCoords,baseCoords)
     check()
 
 
@@ -99,12 +111,12 @@ def moveright(event):
         PCoords[2] = PCoords[2] - 50
         del path[-1]
     can.coords(player, PCoords[0], PCoords[1], PCoords[2], PCoords[3])
-    print(path)
+    print(path,PCoords,baseCoords)
     check()
 
 
 def blink():
-    current = can.itemcget(player, "state")                     #le perso clignote wouhou
+    current = can.itemcget(player, "state")  # le perso clignote wouhou
     next = 'hidden' if current == 'normal' else 'normal'
     can.itemconfig(player, state=next)
     fenetre.after(500, blink)
@@ -119,4 +131,4 @@ fenetre.bind('<Left>', moveleft)
 fenetre.bind('<Right>', moveright)
 fenetre.focus_set()
 blink()
-fenetre.mainloop()
+fenetre.mainloop() n,n 
